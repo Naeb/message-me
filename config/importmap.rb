156939af88852630d29fb82151e94b90379dde2a
Guilherme Berlintes <guilherme.berlintes@outlook.com>
2022-12-20 16:15:36 -0300

    Get Semantic-UI to work on Rails 7

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,7 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "jquery", to: "jquery.min.js", preload: true
+pin "semantic-ui", to: "semantic-ui.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "styles"
